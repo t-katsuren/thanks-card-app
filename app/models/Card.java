3463e8bbd79438c1c541dc2f34c730311326920f
User <User@ETP51-4A-033>
2016-05-23 13:36:23 +0900

    感謝カードテーブル、部門テーブル、分類テーブル作成しました

---
+++ app/models/Card.java
@@ -0,0 +1,50 @@
+package models;
+
+import javax.persistence.*;
+
+
+import com.avaje.ebean.Model;
+
+import play.data.validation.Constraints.MaxLength;
+import play.data.validation.Constraints.Required;
+
+@Entity
+public class Card extends Model {
+
+	@Id
+	public Integer cardId;
+
+	@Required
+	public Date date;
+
+	@Required
+	@ManyToOne
+	public Integer fromUserId;
+
+	@Required
+	@ManyToOne
+	public Integer toUserId;
+
+	@Required
+	@MaxLength(30)
+	public String title;
+
+	@Required
+	@MaxLength(60)
+	public String detail;
+
+	@Required
+	@MaxLength(150)
+	public String message;
+
+	@Required
+	@ManyToOne
+	public Integer categoryId;
+
+	@Required
+	public Integer goodCount;
+
+	public static Find<Integer, Card> find = new Find<Integer, Card>(){};
+
+}
+
