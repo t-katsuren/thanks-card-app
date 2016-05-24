f250a147f025ec852bae704ac0882b6186c49e1a
tekky <tekky@stars_VAIO>
2016-05-24 20:18:19 +0900

    インデント調整

--- app/controllers/Secured.java
+++ app/controllers/Secured.java
@@ -6,13 +6,13 @@ import play.mvc.Http.Context;
 public class Secured extends Security.Authenticator {
 
 	@Override
-    public String getUsername(Context ctx) {
-        return ctx.session().get("login");
-    }
+	public String getUsername(Context ctx) {
+		return ctx.session().get("login");
+	}
 
-    @Override
-    public Result onUnauthorized(Context ctx) {
-        return redirect(routes.AuthController.login());
-    }
+	@Override
+	public Result onUnauthorized(Context ctx) {
+		return redirect(routes.AuthController.login());
+	}
 
 }
