086fd4479e086a7f10db480f58b0d2b6317fabe4
iTAP <iTAP@iTAP-PC19>
2016-05-30 12:42:02 +0900

    掲示板一覧画面のフィルタ機能 -> 未完了だけど一旦コミット

--- app/controllers/ModelController.java
+++ app/controllers/ModelController.java
@@ -32,6 +32,30 @@ public class ModelController extends Controller {
 
 			List<Card> cards = Card.find.all();
 
+			Map<String, String[]> params = request().body().asFormUrlEncoded();
+
+			if(params != null) {
+
+				if(params.get("fromDepartmentName")[0] != null) {
+					Department department = Department.find.where().eq("departmentName", params.get("fromDepartmentName")[0]).findUnique();
+					List<User> fromUserList = User.find.where().eq("department", department).findList();
+					List<Card> temp = new ArrayList<>();
+					for(int i = 0; i < fromUserList.size(); i++) {
+						List<Card> temp1 = Card.find.where().eq("fromUser", fromUserList.get(i)).findList();
+						for(int j = 0; j < temp1.size(); j++) {
+							temp.add(temp1.get(j));
+						}
+					}
+					cards = temp;
+				}
+
+				if(params.get("fromUserName")[0] != null) {
+					User fromUser = User.find.where().eq("userName", params.get("fromUserName")[0]).findUnique();
+					cards = Card.find.where().eq("fromUser", fromUser).findList();
+				}
+
+			}
+
 			List<String[]> cardList = new ArrayList<>();
 
 			SimpleDateFormat formatter = new SimpleDateFormat("yyyy/MM/dd hh:mm:ss");
@@ -51,7 +75,11 @@ public class ModelController extends Controller {
 				cardList.add(temp);
 			}
 
-			return ok(bbs_cont1.render(cardList));
+			List<Department> departmentList = Department.find.all();
+
+			List<Category> categoryList = Category.find.all();
+
+			return ok(bbs_cont1.render(cardList, departmentList, categoryList));
 
 		}
 		//掲示板事例1
