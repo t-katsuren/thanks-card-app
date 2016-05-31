4792cdf6c4cb302898b3bd4b26ddfa91ff042ed3
User <User@ETP51-4A-032>
2016-05-31 09:59:53 +0900

    bbs, mypage for文の変更

--- app/controllers/BbsController.java
+++ app/controllers/BbsController.java
@@ -130,7 +130,7 @@ public class BbsController extends Controller {
 
 		SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd hh:mm:ss");
 
-		for(int i = 0; i < cards.size(); i++) {
+		for(int i = cards.size()-1; i >=0; i--) {
 			String[] temp = new String[9];
 
 			temp[0] = String.valueOf(cards.get(i).id);
